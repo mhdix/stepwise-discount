3d26c256ca62b1343986993d52c27ef52f34e470
mhdix <domanloom@gmail.com>
2024-10-04 22:00:51 -0700

    complated all bugs and logic

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ function calculateDiscount (price) {
     const minPrice = 100
     const discountSteps = 3
     const maxDiscount = 25
-    
+
     if(price < minPrice + discountPerPrice) return 'no discount'
     let discount = Math.floor((price - minPrice) / discountPerPrice) * discountSteps;
-    if(discount > maxDiscount ) return discount = maxDiscount
-    return discount
+    if(discount > maxDiscount )  {
+        discount = maxDiscount
+        return discount + '%'
+        
+    }
+    return discount + '%'
 }
-console.log(calculateDiscount(110))
+console.log(calculateDiscount(150))
