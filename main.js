8a055f9f674e8feacdcf8d496310ac5440056b3b
mhdix <domanloom@gmail.com>
2024-10-04 21:25:52 -0700

    complate stepwise discount logic

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-    
+function calculateDiscount (price) {
+    if(price < 100 + 10) return 'no discount'
+    let discount = Math.floor((price - 100) / 10) * 3;
+    if(discount > 25 ) return discount = 25
+    return discount
+}
+console.log()
